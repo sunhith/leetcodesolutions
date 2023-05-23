3bb9d3fe5b9b90a1b531694d68b2e70204036e72
sunhith <55741107+sunhith@users.noreply.github.com>
2023-05-23 01:36:46 -0500

    Time: 2 ms (100.00%), Space: 59.4 MB (7.85%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int n = stones.length;
+        if(n == 2) return stones[1] - stones[0];
+        int max = 0;
+        for(int i = 0; i < n-2; i++){
+            max = Math.max(max, stones[i+2] - stones[i]);
+        }
+        return max;
+    }
+}

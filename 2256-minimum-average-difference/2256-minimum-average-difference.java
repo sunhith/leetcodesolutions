a4d5b1b1aed91ae336f23a5f7de39cc6c599cdf0
sunhith <55741107+sunhith@users.noreply.github.com>
2023-05-24 19:35:15 -0500

    Time: 24 ms (7.07%), Space: 61.5 MB (63.64%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,15 +1,25 @@
 class Solution {
     public int minimumAverageDifference(int[] nums) 
-    {
-        
-        
-        
+    {        
         int l = nums.length;  
         int index = 0;
         long min = Integer.MAX_VALUE, sum = 0, leftsum = 0, rightsum = 0;
-        for (int j = 0; j < l; j++) {
-            sum = sum + nums[j];
+        System.out.println("length "+l);
+        
+        
+        if(l%2 == 0){
+            for (int j = 0; j < l/2; j++) {
+                sum = sum + nums[j] + nums[l-j-1];
+            }
         }
+        else{
+            int j;
+            for (j = 0; j < l/2; j++) {
+                sum = sum + nums[j] + nums[l-j-1];
+            }
+            sum += nums[j]; 
+        }
+        
         for (int i = 0; i < l; i++) {
             leftsum += nums[i];
             rightsum = sum - leftsum;
@@ -19,33 +29,6 @@ class Solution {
                 index = i;
             }
         }
-        return index;
-        
-//         int n = nums.length;
-//         System.out.println(n);
-//         float a[] = new float[n]; // stores i+1 elements of nums average at ith index
-//         float b[] = new float[n]; // stores n-i-1 elements of nums average at ith index
-//         int min = Integer.MAX_VALUE;
-//         int index = 0;
-        
-//         a[0] = nums[0];
-//         b[n-1] = 0;
-//         for(int i = 1; i < n; i++){
-//             a[i] = (i * a[i-1] + nums[i])/(i + 1);
-//             b[n-i-1] = ((i-1) * b[n-i] + nums[n-i])/(i);
-//         }
-        
-        
-//         for(int i = 0; i < n; i++)
-//         {
-//             int diff = (int)Math.abs(Math.floor(a[i]) - Math.floor(b[i]));
-//             if(min > diff)
-//             {
-//                 min = diff;
-//                 index = i;
-//             }
-//         }
-        
-//         return index;
+        return index;  
     }
 }

056285ef69b47729cdc0e0b26ae8e8ac563d7d49
sunhith <55741107+sunhith@users.noreply.github.com>
2023-05-25 01:11:49 -0500

    Time: 4 ms (91.30%), Space: 42.1 MB (99.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,24 +1,32 @@
 import java.util.*;
 import java.lang.*;
 class Solution {
+    // Sliding window technique
     public int lengthOfLongestSubstring(String s) {
       int n = s.length();
         if(n == 1 || n==0) return n;
         boolean charVisited[] = new boolean[256];
-        int i = 0, j = 1, max = 1;
+        int i = 0, j = 1, max = 1; // i-> left pntr; j-> right pntr
         charVisited[s.charAt(0)] = true;
+        
         while(j < n){
-
+            // if character is repeated
             if(charVisited[s.charAt(j)]){
+                // move left ptr till left and right ptr chars are equal and also mark char at ith position as unvisited
                 while( !((Character)s.charAt(i)).equals(s.charAt(j))){
                     charVisited[s.charAt(i)] = false;
                     i++;
                 }
+                // move the left pntr by 1 since left and right ptr chars are equal
                 i++;
             }
 
+            //if new substring length is greater than max
             if(j-i+1 > max){
                 max = j-i+1;
+                // for printing substring
+                // startIndex = i
+                // endIndex = j
             }
             charVisited[s.charAt(j)] = true;
             j++;

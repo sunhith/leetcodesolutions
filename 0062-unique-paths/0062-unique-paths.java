79ff8f224d03a61d7a9690e7e597aaf7cc709f2d
sunhith <55741107+sunhith@users.noreply.github.com>
2023-06-22 00:50:38 -0500

    Time: 0 ms (100.00%), Space: 39.5 MB (64.16%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if(m==1 && n == 1) return 1;
+        int dp[][] = new int[m][n];
+        Arrays.fill(dp[0], 1);
+        for(int row = 1; row < m; row++){
+            dp[row][0] = 1;
+        }
+        
+        dp[0][0] = 0;
+        
+        for(int row = 1; row < m; row++){
+            for(int col = 1; col < n; col++)
+            {
+                dp[row][col] = dp[row-1][col] + dp[row][col-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+}

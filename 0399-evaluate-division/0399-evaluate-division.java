ee0e53ddea986cf3911cb0df25c09b3330453e74
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2023-07-01 19:09:44 -0500

    Time: 3 ms (6.00%), Space: 41.3 MB (54.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -3,97 +3,185 @@ import java.util.Map.*;
 import java.util.concurrent.atomic.*;
 class Solution {
     
-      public static void createGraph(List<List<String>> equations, double[] values, HashMap<String, HashMap<String, Double>> graph)
-    {
-        AtomicInteger count = new AtomicInteger(0);
-        equations.stream().forEach(
-                res -> {
-                    String from = res.get(0);
-                    String to =res.get(1);
-                    int counter = count.getAndIncrement();
-                    if(graph.get(from) == null)
-                    {
-                        HashMap<String, Double> map = new HashMap<>();
-                        map.put(to, values[counter]);
-                        graph.put(from, map);
-                    }
-                    else {
-                        HashMap<String, Double> map = graph.get(from);
-                        map.put(to, values[counter]);
-                        graph.put(from, map);
-                    }
+   public static class Union {
 
-                    // bi-directional graph creation with opposite side having edge as 1/value
-                    HashMap<String, Double> map;
-                    if(graph.get(to) == null){
-                        map = new HashMap<>();
-                        map.put(from, 1/values[counter]);
-                        graph.put(to, map);
-                    }
-                    else {
-                        map = graph.get(to);
-                        map.put(from, 1/values[counter]);
-                        graph.put(to, map);
-                    }
-                }
-        );
-    }
+        int parent[];
+        double weight[];
 
-    public static Double DFS(HashMap<String, HashMap<String, Double>> graph, String src, String des)
-    {
-        HashSet<String> visited =  new HashSet<>();
-        HashMap<String, Double> vertices = graph.get(src);
-        HashMap<String, String> parent = new HashMap<>();
-        Deque<String> stack = new ArrayDeque<>();
+        Union(int size) {
+            parent = new int[size];
+            weight = new double[size];
 
-        if(vertices == null) return Double.valueOf(-1);
-        stack.push(src);
-
-        while (!stack.isEmpty())
-        {
-            String u = stack.pop();
-            vertices = graph.get(u); // get out edges of u
-            if(vertices == null) return Double.valueOf(-1);
-            for(Entry<String, Double> entry: vertices.entrySet()){
-                if(des.equals(entry.getKey())){
-                    String from = entry.getKey();
-                    String to = u;
-                    Double result = 1d;
-
-                    while (!src.equals(from))
-                    {
-                        result *= graph.get(from).get(to);
-                        from = to;
-                        to = parent.get(to);
-                    }
-                    parent.put(entry.getKey(), u);
-                    return 1/result;
-                }
-                if(!visited.contains(entry.getKey()))
-                {
-                    stack.push(entry.getKey());
-                    parent.put(entry.getKey(), u);
-                    visited.add(entry.getKey());
-                }
-
+            // initialize parents and weights
+            for (int i = 0; i < size; i++) {
+                parent[i] = i;
+                weight[i] = 1.0;
             }
         }
 
-        return Double.valueOf(-1);
+        public void findUnion(int p, int q, double value) {
+            int ultimateParentp = findParent(p); // x
+            int ultimateParentq = findParent(q); // y
+
+            if (ultimateParentp != ultimateParentq) {
+                parent[ultimateParentp] = ultimateParentq;
+                weight[ultimateParentp] = value * weight[q] / weight[p]; // (x/y) = value * (wq/wp)
+            }
+        }
+
+        public int findParent(int p) {
+            if (parent[p] != p) {
+                int origin = parent[p];
+                parent[p] = findParent(origin);
+                weight[p] *= weight[origin];
+            }
+            return parent[p];
+        }
+
+        // returns p/q values
+        public double isConnected(int p, int q) {
+            int ultimateParentp = findParent(p);
+            int ultimateParentq = findParent(q);
+            if (ultimateParentp == ultimateParentq)
+                return weight[p] / weight[q];
+            return -1.0;
+        }
+
+    }
+
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicInteger count2 = new AtomicInteger(0);
+        Union union = new Union(equations.size() * 2);
+
+
+        HashMap<String, Integer> map = new HashMap<>();
+        equations.
+        stream().
+        forEach(res ->
+                {
+                    String from = res.get(0);
+                    String to = res.get(1);
+                    if (map.get(from) == null)
+                        map.put(from, count.getAndIncrement());
+                    if (map.get(to) == null)
+                        map.put(to, count.getAndIncrement());
+                    union.findUnion(map.get(from), map.get(to), values[count2.getAndIncrement()]);
+                }
+                );
+
+        AtomicInteger count3 = new AtomicInteger(0);
+        double result[] = new double[queries.size()];
+        queries.
+                stream().
+                forEach(res ->
+                        {
+                            String from = res.get(0);
+                            String to = res.get(1);
+                            if (!map.containsKey(from) || !map.containsKey(to)) {
+                                result[count3.getAndIncrement()] = -1.0;
+                            }
+                            else
+                             result[count3.getAndIncrement()] = union.isConnected(map.get(from), map.get(to));
+                        }
+                );
+        return result;
     }
     
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) 
-    {
-        HashMap<String, HashMap<String, Double>> graph = new HashMap<>();
-        createGraph(equations, values, graph);
-        double res[] = new double[queries.size()];
-        for(int i = 0; i<queries.size(); i++)
-        {
-            String src = queries.get(i).get(0);
-            String des = queries.get(i).get(1);
-            res[i] = DFS(graph, src, des);
-        }
-        return res;
+    /** Method-2 - Not optimized - running DFS for each query
+    **/
+//       public static void createGraph(List<List<String>> equations, double[] values, HashMap<String, HashMap<String, Double>> graph)
+//     {
+//         AtomicInteger count = new AtomicInteger(0);
+//         equations.stream().forEach(
+//                 res -> {
+//                     String from = res.get(0);
+//                     String to =res.get(1);
+//                     int counter = count.getAndIncrement();
+//                     if(graph.get(from) == null)
+//                     {
+//                         HashMap<String, Double> map = new HashMap<>();
+//                         map.put(to, values[counter]);
+//                         graph.put(from, map);
+//                     }
+//                     else {
+//                         HashMap<String, Double> map = graph.get(from);
+//                         map.put(to, values[counter]);
+//                         graph.put(from, map);
+//                     }
+
+//                     // bi-directional graph creation with opposite side having edge as 1/value
+//                     HashMap<String, Double> map;
+//                     if(graph.get(to) == null){
+//                         map = new HashMap<>();
+//                         map.put(from, 1/values[counter]);
+//                         graph.put(to, map);
+//                     }
+//                     else {
+//                         map = graph.get(to);
+//                         map.put(from, 1/values[counter]);
+//                         graph.put(to, map);
+//                     }
+//                 }
+//         );
+//     }
+
+//     public static Double DFS(HashMap<String, HashMap<String, Double>> graph, String src, String des)
+//     {
+//         HashSet<String> visited =  new HashSet<>();
+//         HashMap<String, Double> vertices = graph.get(src);
+//         HashMap<String, String> parent = new HashMap<>();
+//         Deque<String> stack = new ArrayDeque<>();
+
+//         if(vertices == null) return Double.valueOf(-1);
+//         stack.push(src);
+
+//         while (!stack.isEmpty())
+//         {
+//             String u = stack.pop();
+//             vertices = graph.get(u); // get out edges of u
+//             if(vertices == null) return Double.valueOf(-1);
+//             for(Entry<String, Double> entry: vertices.entrySet()){
+//                 if(des.equals(entry.getKey())){
+//                     String from = entry.getKey();
+//                     String to = u;
+//                     Double result = 1d;
+
+//                     while (!src.equals(from))
+//                     {
+//                         result *= graph.get(from).get(to);
+//                         from = to;
+//                         to = parent.get(to);
+//                     }
+//                     parent.put(entry.getKey(), u);
+//                     return 1/result;
+//                 }
+//                 if(!visited.contains(entry.getKey()))
+//                 {
+//                     stack.push(entry.getKey());
+//                     parent.put(entry.getKey(), u);
+//                     visited.add(entry.getKey());
+//                 }
+
+//             }
+//         }
+
+//         return Double.valueOf(-1);
+//     }
+    
+//     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) 
+//     {
+//         HashMap<String, HashMap<String, Double>> graph = new HashMap<>();
+//         createGraph(equations, values, graph);
+//         double res[] = new double[queries.size()];
+//         for(int i = 0; i<queries.size(); i++)
+//         {
+//             String src = queries.get(i).get(0);
+//             String des = queries.get(i).get(1);
+//             res[i] = DFS(graph, src, des);
+//         }
+//         return res;
         
-    }
+//     }
 }

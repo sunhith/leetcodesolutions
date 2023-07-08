b28e1a5e88f03781f4b05e800163419ba0373ac3
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2023-07-08 00:54:46 -0500

    Time: 8 ms (100.00%), Space: 44.6 MB (41.93%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.java
@@ -1,21 +1,37 @@
 import java.util.*;
 class Solution {
     public String makeSmallestPalindrome(String s) {
-        int n = s.length();
-        StringBuilder str = new StringBuilder(s);
-        int left = 0, right = n-1;
-        while(left < right){
-        if(s.charAt(left) != s.charAt(right)){
-            if(s.charAt(left) > s.charAt(right)){
-                str.setCharAt(left, s.charAt(right)); 
+        char[] arr = s.toCharArray();
+        int i=0;
+        int j = arr.length - 1;
+        while(i < j){
+            if(arr[i] != arr[j]){
+                char ch = (char)Math.min(arr[i], arr[j]);
+                arr[i] = ch;
+                arr[j] = ch;
             }
-            else{
-                str.setCharAt(right, s.charAt(left)); 
-            }
+            i++;
+            j--;
+            
         }
-             left++;
-             right--;
-        }
-        return str.toString();
+        
+        return String.valueOf(arr);
+        
+        // int n = s.length();
+        // StringBuilder str = new StringBuilder(s);
+        // int left = 0, right = n-1;
+        // while(left < right){
+        // if(s.charAt(left) != s.charAt(right)){
+        //     if(s.charAt(left) > s.charAt(right)){
+        //         str.setCharAt(left, s.charAt(right)); 
+        //     }
+        //     else{
+        //         str.setCharAt(right, s.charAt(left)); 
+        //     }
+        // }
+        //      left++;
+        //      right--;
+        // }
+        // return str.toString();
     }
 }

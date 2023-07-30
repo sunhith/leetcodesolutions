bd0fda8ff2e5f5344f194777dc4678e509a32ce6
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2023-07-30 00:48:05 -0500

    Time: 31 ms (98.64%), Space: 50.6 MB (84.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -8,6 +8,8 @@ class Solution {
         int i,j,k;
         for ( i= 0; i < n; i++)
         {
+            if (i != 0 && nums[i] == nums[i - 1])
+                        continue;
             j = i+1;
             k = n-1;
             while(j < k)

aff44c5c1cf5792244ece64050a69739a463a5d8
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2023-08-07 16:18:03 -0500

    Time: 21 ms (68.36%), Space: 43.9 MB (82.59%) - LeetHub

---
+++ 0018-4sum/0018-4sum.java
@@ -0,0 +1,54 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i <= n-4; i++ )
+        {
+            if (i != 0 && nums[i] == nums[i - 1])
+                        continue;
+            for(int j = i+1; j <= n-3; j++){
+                
+                if (j != i+1 && nums[j] == nums[j - 1])
+                        continue;
+                int k = j+1;
+                int l = n-1;
+                while(k < l)
+                {
+                    long sum = nums[i] ;
+                    sum+= nums[j];
+                    sum+= nums[k];
+                    sum+= nums[l];
+                    
+                    if(sum < target){
+                        k++;
+                    }
+                    else if(sum > target){
+                        l--;
+                    }
+                    else
+                    {
+                        System.out.println(sum);
+                        List<Integer> sum4 = new ArrayList(4);
+                        sum4.add(nums[i]);
+                        sum4.add(nums[j]);
+                        sum4.add(nums[k]);
+                        sum4.add(nums[l]);
+                        list.add(sum4);
+                        k++;
+                        l--;
+                        while(k < n && nums[k] == nums[k - 1])
+                        {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l + 1])
+                        {
+                            l--;
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+}

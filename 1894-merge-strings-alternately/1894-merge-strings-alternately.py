8ac94e60b9e9160bad9ffdf50a89033a3c2e9081
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-02-10 21:12:08 -0600

    Time: 39 ms (48.91%), Space: 17.8 MB (30.9%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -9,15 +9,4 @@ class Solution:
             if l2>i:
                 s = s+word2[i]
         return s
-        # while(i < l1 and j <l2):
-        #     s = s + word1[i]+word2[j]
-        #     i = i+1
-        #     j = j+1
-        # while i < l1:
-        #     s = s+ word1[i]
-        #     i = i + 1
-        # while j < l2:
-        #     s = s+word2[j]
-        #     j = j+1
-        # return s
             
